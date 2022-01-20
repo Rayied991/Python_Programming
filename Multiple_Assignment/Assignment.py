656ef53b9a7e040cabca22a4a3885f459aedea50
MD.SHAHZAD HUSSAIN RAYIED <76652122+Rayied991@users.noreply.github.com>
2022-01-20 22:30:38 +0600

    Add files via upload

---
+++ Multiple_Assignment/Assignment.py
@@ -0,0 +1,12 @@
+#Multiple_Assignment=allows us to assign multiple variables at the same time in one line of code
+
+# name="Rayied"
+# age=21
+
+name,age="Rayied",21
+print("I am ",name," and  My age =",age)
+
+rayied=alif =asif=20;
+print(rayied)
+print(alif)
+print(asif)

c20a99684d2ffd4c5dab763e0a6ef572e8a673a2
MD.SHAHZAD HUSSAIN RAYIED <76652122+Rayied991@users.noreply.github.com>
2022-01-21 21:41:36 +0600

    Update Math.py

--- Math_Class/Math.py
+++ Math_Class/Math.py
@@ -1,4 +1,4 @@
-import math
+import math#-> math module
 # Python has also a built-in module called math, which extends the list of mathematical functions
 # The min() and max() functions can be used to find the lowest or highest value in an iterable
 
@@ -8,13 +8,15 @@ y=max(25,30,35,40,45)
 print("Minimum value is :",x)
 print("Maximum value is :",y)
 
+#factorial
+print("Factorial of 7 :",math.factorial(7))
 # The abs() function returns the absolute (positive) value of the specified number
 
 z=abs(-7.25)
 print("Absolute Value : ",z)
 
 #pow function
-a=pow(4,3)
+a=pow(4,3)#base =4 , exponent=3
 print("4^3 = ",a)
 
 #sqrt->returns the square root of a number
@@ -23,12 +25,13 @@ print("Square root of 16 = ",b)
 
 #ceil->rounds a number upwards to its nearest integer
 #floor->rounds a number downwards to its nearest integer
+#round->rounds a number to the nearest integer
 
-c=math.ceil(1.4)
-d=math.floor(1.4)
 
-print("Ceil value : ",c)#returns 2
-print("Floor value : ",d)#returns 1
+
+print("Ceil value : ",math.ceil(math.pi))#returns 2
+print("Floor value : ",math.floor(math.pi))#returns 1
+print("Round value of Pi : ",round(math.pi))#returns 3
 
 #Pi=3.1416
 p=math.pi
